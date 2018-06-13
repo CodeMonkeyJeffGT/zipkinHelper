678346aec2c68d1669b8bed4d33d7f57b2f0cb56
CodeMonkeyJeffGT <18846938586@163.com>
2018-06-13 16:07:39 +0800

    删除多余输出

--- src/Zipkin.php
+++ src/Zipkin.php
@@ -130,8 +130,7 @@ class Zipkin
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-        echo $result;
+        curl_exec($ch);
         curl_close($ch);
     }
 
